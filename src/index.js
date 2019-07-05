d616fdcf38fc23f0319edbf06276005046ea610b
wangchuan <chuan.wang@okcoin.com>
2019-07-05 16:17:27 +0800

    ci 测试3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.less';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const a = 'Chuan test 12';
+  const a = 'Chuan test 3';
   document.body.innerHTML = a;
 }, false);
