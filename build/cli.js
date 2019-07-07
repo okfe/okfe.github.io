ab788dedb37273d98323ad8c58ff52c2f3326e96
VarHug <490641570@qq.com>
2019-07-07 10:12:53 +0800

    改变issuse地址为个人仓库测试

--- build/cli.js
+++ build/cli.js
@@ -3,8 +3,8 @@ const md = require('markdown-it')();
 const fs = require('fs');
 const path = require('path');
 
-const labelsUrl = 'https://api.github.com/repos/okfe/weekly/labels';
-const issuesUrl = 'https://api.github.com/repos/okfe/weekly/issues';
+const labelsUrl = 'https://api.github.com/repos/VarHug/test/labels';
+const issuesUrl = 'https://api.github.com/repos/VarHug/test/issues';
 
 const distPath = path.resolve(__dirname, '../api');
 const labelsFileName = path.resolve(distPath, 'labelList.json');
