494bc4702ee6ee5ded109ccb18b371b3f95ce68a
VarHug <490641570@qq.com>
2019-07-12 10:35:05 +0800

    t

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.less';
+import issueListJson from '../api/issueList.json';
 
 document.addEventListener("DOMContentLoaded", () => {
   const a = 'test';
-  console.log(a);
+  console.log(issueListJson.data);
   document.body.innerHTML = a;
+  $('body').append('<div><a href="../api/464908294.json">issue</a><div>');
 }, false);
