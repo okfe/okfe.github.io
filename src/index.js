81505d2de783a391c2f4a3e45e700cbe7c88176d
isJaakko <1512803764@qq.com>
2019-07-14 20:19:59 +0800

    首页卡片

--- src/index.js
+++ src/index.js
@@ -18,17 +18,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let fragments = '';
   issueList.forEach((issue) => {
-    fragments += `<div class="issue-title" data-id=${issue.id}>${issue.title}</div>`;
+    // fragments += `<div class="issue-title" data-id=${issue.id}>${issue.title}</div>`;
+    fragments +=
+      `
+    <div class="article-card">
+      <span class="article-title">${issue.title}</span>
+      <div class="article-info">
+        <span class="article-author">${issue.author || '暂无作者信息'}</span>
+        <span class="article-date">${issue.created_at}</span>
+      </div>
+      <p class="article-outline">
+        ${issue.outline || '暂无文章缩略信息'}
+      </p>
+      <div class="article-detail" data-id="${issue.id}">
+        继续阅读<i class="fa fa-angle-double-right fa-lg"></i>
+      </div>
+    </div>`
     route.route(`${issue.id}`, viewFn.bind(null, `/${issue.id}`));
   });
 
   route.route('/', () => {
     $('#app').html(fragments);
-    const $issueList = $('.issue-title');
+    const $issueList = $('.article-detail');
     $issueList.on('click', (ev) => {
       const id = $(ev.target).data('id')
       location.hash = id;
     });
   });
-  
+
 }, false);
