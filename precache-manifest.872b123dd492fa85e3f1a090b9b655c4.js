b30097b681c23ebe23ba44a7fc381591eca37b4a
VarHug <490641570@qq.com>
2019-10-11 18:44:46 +0800

    Updates

---
+++ precache-manifest.872b123dd492fa85e3f1a090b9b655c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9807f71966017ca9fe4299982f550949",
+    "url": "/404.html"
+  },
+  {
+    "revision": "9807f71966017ca9fe4299982f550949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fa4a2823fa38f5f135",
+    "url": "/static/css/2.3946a297.chunk.css"
+  },
+  {
+    "revision": "e2fa4a2823fa38f5f135",
+    "url": "/static/js/2.d2edca9f.chunk.js"
+  },
+  {
+    "revision": "63edddf386665409bbde",
+    "url": "/static/js/main.61e43981.chunk.js"
+  },
+  {
+    "revision": "4cef2887cdbadde52394",
+    "url": "/static/js/runtime~main.c7ffcae4.js"
+  }
+]);
