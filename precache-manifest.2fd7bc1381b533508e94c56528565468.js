d2f7ff9a4485a1b1a60ff75edaffad17cccca07b
isJaakko <1512803764@qq.com>
2019-10-16 00:20:24 +0800

    Updates

---
+++ precache-manifest.2fd7bc1381b533508e94c56528565468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea755bb4f8cdf30db9c1cdb6371dbf56",
+    "url": "/404.html"
+  },
+  {
+    "revision": "ea755bb4f8cdf30db9c1cdb6371dbf56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded08657e0af2a0d38f7",
+    "url": "/static/css/2.9ebce09b.chunk.css"
+  },
+  {
+    "revision": "ded08657e0af2a0d38f7",
+    "url": "/static/js/2.37453ac8.chunk.js"
+  },
+  {
+    "revision": "1b94ef5455c875c58d11",
+    "url": "/static/js/main.cb9279bd.chunk.js"
+  },
+  {
+    "revision": "4cef2887cdbadde52394",
+    "url": "/static/js/runtime~main.c7ffcae4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f32c8a2b1c106283fa867588ab3e34f.js"
+  "/precache-manifest.2fd7bc1381b533508e94c56528565468.js"
 );
 
 self.addEventListener('message', (event) => {
