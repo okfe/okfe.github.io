8b79c7efa9abe55f0c057c5a308124bc1c75061c
isJaakko <1512803764@qq.com>
2019-10-16 11:52:55 +0800

    markdown 样式改进

--- src/components/ReactMarkdown/index.js
+++ src/components/ReactMarkdown/index.js
@@ -5,6 +5,7 @@ import CodeBlock from '_src/components/CodeBlock';
 export default class Markdown extends React.PureComponent {
   render() {
     const { source } = this.props;
+
     return (
       <ReactMarkdown className={'markdown'}
         //必须是false不然img标签渲染不出来
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Router from './router';
 import * as serviceWorker from './serviceWorker';
 import '_src/less/style.less';
 import '_src/less/markdown.less';
-import '_src/less/hignlignt.less';
+import '_src/less/highlight.less';
 import store from '_src/store';
 
 ReactDOM.render(
